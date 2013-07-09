2c75bfe9b25f6ee24d77848f6d50513d40c7a5f9
Ubuntu <ubuntu@ip-172-31-5-210.us-west-2.compute.internal>
2013-07-09 03:22:10 +0000

    Playing More

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 //var buffer = new Buffer();
 
-//var contents = buf.toString(fs.readFileSync('index.html'));
+var contents = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send("Hello World updating");
